73870c14826f8c9b35e3a8bf518815f8b140e30c
MosesChoice <ogwahchoice124@gmail.com>
2023-02-24 12:54:04 -0800

    corrected to 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,14 @@
  */
 void print_alphabet_x10(void)
 {
-	int i = 0;
-	char c;
-
-	while (i++ <= 18)
-	{
-	for (c = 'a'; c <= 'z'; c++)
-	_putchar(c);
-	{
-	_putchar('\n');
+      int i = 0;
+      char c;
+    while (i++ <= 18)
+    {
+      for (c = 'a'; c <= 'z'; c++)
+      _putchar(c);
+     {
+     _putchar('\n');
 	}
 	i++;
 	}
